f0ceedf89bd7f482555987671a6a1a1810390876
joydeep-git <joydeep.accounts@tuta.io>
2025-12-19 15:37:54 +0530

    added image configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+        pathname: '/**',
+      },
+      {
+        protocol: "https",
+        hostname: "newsglance-s3.s3.ap-south-1.amazonaws.com",
+        pathname: "/**",
+      },
+    ]
+  }
 };
 
 export default nextConfig;
