393860e2ceadd56a1c96aab31aee81fc6975f761
joydeep-git <joydeep.accounts@tuta.io>
2026-03-17 22:16:50 +0530

    added image URL in next.config

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,11 @@ const nextConfig: NextConfig = {
         hostname: "picsum.photos",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "media.guim.co.uk",
+        pathname: "/**", 
+      }
     ]
   }
 };
