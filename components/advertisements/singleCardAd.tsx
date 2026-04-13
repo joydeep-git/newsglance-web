a1d1d3cd2873f0f6db87f6929ed20e69388bbf60
joydeep-git <joydeep.accounts@tuta.io>
2026-04-14 05:28:05 +0530

    upgraded ads

--- components/advertisements/singleCardAd.tsx
+++ components/advertisements/singleCardAd.tsx
@@ -1,22 +1,54 @@
+"use client";
+
+import { useEffect, useRef } from "react";
 import { useAppSelector } from "@/redux/store";
 import AdContainer from "./adContainer";
 
+declare global {
+  interface Window {
+    aclib?: {
+      runBanner: (options: { zoneId: string }) => void;
+    };
+  }
+}
 
 const SingleCardAd = () => {
 
-  const { user } = useAppSelector(s => s.auth);
+  const { user } = useAppSelector((s) => s.auth);
+  const adRef = useRef<HTMLDivElement>(null);
+  const initialized = useRef(false);
 
+  useEffect(() => {
+    if (initialized.current) return;
+
+    const run = () => {
+      if (typeof window.aclib !== "undefined" && adRef.current) {
+        initialized.current = true;
+        window.aclib.runBanner({ zoneId: "11188954" });
+      }
+    };
+
+    if (typeof window.aclib !== "undefined") {
+      run();
+    } else {
+      const interval = setInterval(() => {
+        if (typeof window.aclib !== "undefined") {
+          clearInterval(interval);
+          run();
+        }
+      }, 100);
+
+      return () => clearInterval(interval);
+    }
+  }, []);
 
   if (user && user?.isPremium) return null;
 
   return (
     <AdContainer>
-      <div dangerouslySetInnerHTML={{
-        __html: `<script type="text/javascript">aclib.runBanner({zoneId: '11188954' })</script>`
-      }} />
+      <div ref={adRef} />
     </AdContainer>
-  )
-
+  );
 };
 
 export default SingleCardAd;
